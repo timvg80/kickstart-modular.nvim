9ac8e5d39b0d25b821abe1697d3d5ee4bc020132
Tim Vivian-Griffiths <tim.vivian.griffiths@tutanota.com>
2024-03-18 11:25:06 +0000

    Getting started with pyright

--- init.lua
+++ init.lua
@@ -105,5 +105,7 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
+require 'custom.config.alpha'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/custom/plugins/alpha.lua
@@ -0,0 +1,4 @@
+return {
+  'goolord/alpha-nvim',
+  lazy = true,
+}
